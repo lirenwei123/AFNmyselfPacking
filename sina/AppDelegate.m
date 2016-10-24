9d07d2ee9515b1714365e169f1326fe07581891a
Capital <303624150@qq.com>
2016-10-25 03:01:24 +0800

    afn的封装以及sina的api使用

--- sina/AppDelegate.m
+++ sina/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "LRWHttpKit.h"
 
 @interface AppDelegate ()
 
@@ -16,7 +17,9 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+   
+    [LRWHttpKit obbserverNetStatus];
+    
     return YES;
 }
 
